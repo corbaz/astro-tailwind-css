866e83ee91c68c864d73ccabc9714f0a67871101
Julio Corbaz <julio.corbaz@gmail.com>
2025-04-15 19:18:28 -0300

    v.8.4

--- src/const/appConstants.ts
+++ src/const/appConstants.ts
@@ -1,6 +1,6 @@
 // Constantes globales de la aplicación
 
 // Versión de la aplicación
-export const VERSION = "1.1.0";
+export const VERSION = "8.4.0";
 
 // Puedes exportar otras constantes globales aquí
